9cc5b5183a2ce3c280bd37da56a65e8a16faf4b4
jennubbruh <jenche@my.yorku.ca>
2025-05-21 14:17:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI on Render!"}
+
+import uvicorn
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
